a5a5204ea42ca8e76fedb38b44ed7c77307229d0
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-03-11 19:19:02 -0300

    add SDRAM test, use VHDL as top-level entity
    built and run a SDRAM usage demo/test, reassinged all SDRAM pins
    and switched to using VHDL declared entity as a top-level entity

---
+++ Image_parallel_processing-2proc/image_parallel_processing_qsys/image_parallel_processing_qsys_bb.v
@@ -0,0 +1,30 @@
+
+module image_parallel_processing_qsys (
+	clk_clk,
+	pll_locked_export,
+	reset_reset_n,
+	clk_sdram_clk,
+	sdram_wire_addr,
+	sdram_wire_ba,
+	sdram_wire_cas_n,
+	sdram_wire_cke,
+	sdram_wire_cs_n,
+	sdram_wire_dq,
+	sdram_wire_dqm,
+	sdram_wire_ras_n,
+	sdram_wire_we_n);	
+
+	input		clk_clk;
+	output		pll_locked_export;
+	input		reset_reset_n;
+	output		clk_sdram_clk;
+	output	[12:0]	sdram_wire_addr;
+	output	[1:0]	sdram_wire_ba;
+	output		sdram_wire_cas_n;
+	output		sdram_wire_cke;
+	output		sdram_wire_cs_n;
+	inout	[15:0]	sdram_wire_dq;
+	output	[1:0]	sdram_wire_dqm;
+	output		sdram_wire_ras_n;
+	output		sdram_wire_we_n;
+endmodule
---
+++ Image_parallel_processing-2proc/output_files/Chain1.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 16.1.0 Build 196 10/24/2016 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CSEMA5F31) Path("/home/luctins/repo/NIOS-parallel-processing/Image_parallel_processing-2proc/output_files/") File("image_parallel_processing_time_limited.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
