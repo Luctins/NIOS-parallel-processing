b803771ae00bec92105c8bf6a4e935ba1124caf1
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-03-14 18:23:06 -0300

    add image_fill func; add code skeleton
    added the basic structure for the single processor test

--- Image_parallel_processing-1proc/software/SDRAM-test/main.c
+++ Image_parallel_processing-1proc/software/SDRAM-test/main.c
@@ -3,25 +3,70 @@
 #include "sys/alt_stdio.h"
 #include <stdint.h>
 
+/*--------- Macros/constants -------*/
+#define PROC0_SDRAM_BASE (0x08000000)
+
+#define IMG_H (600)
+#define IMG_W (800)
+
+#define DEBUG_OUTPUT_F alt_printf
+#define DEBUG_OUTPUT alt_putstr
+
+#define DEBUG(msg) 						\
+	DEBUG_OUTPUT(msg);					\
+	DEBUG_OUTPUT("\n")
+
+#define DEBUGF(msg,...)				\
+	DEBUG_OUTPUT_F(msg, __VA_ARGS__ );	\
+	DEBUG_OUTPUT("\n")
+
+#define THRESHOLD (127)
+
+/*------- Function predeclaration --------*/
+
+void fill_image(uint16_t image_base, uint16_t image_height, uint16_t image_width);
+
+/*------- Global Variables --------*/
+
+/*OBS: the image is black and white with 1 byte of color depht*/
+
+
 const uint16_t testvalue = 1234;
 uint16_t testread  = 0;
 
-#define DEBUG(message) alt_printf(message "\n")
 
-#define PROC0_SDRAM_BASE 0x08000000
-int main(void)
+
+/*--------- Main ---------*/
+
+
+void main(void)
 {
+	DEBUG("hello there");
 	while(1)
 	{
-		alt_printf("hello!\n");
-#if 0
-		IOWR_16DIRECT(PROC0_SDRAM_BASE,0,testvalue);
-		for(int i =0; i< 5000000;i++);
-		testread = IORD_16DIRECT(PROC0_SDRAM_BASE,0);
-		alt_printf("value: %i\n", testread);
-		if(!testread)
-			alt_printf("read something");
-#endif
+
 	}
-	return 1;
+}
+
+
+/*---- Function Definition-----------------*/
+/*-----------------------------------------*/
+
+/**
+ * @brief fill image memory range
+ *
+ */
+void fill_image(uint16_t image_base, uint16_t image_height, uint16_t image_width)
+{
+	/*fill image with white to gray gradient*/
+	uint8_t i,j,v;
+	for(i=0;i<image_height;i++) /*write image "rows" sequentially for better memory performance*/
+	{
+		for(j=0;j<image_width;j++)
+		{
+			v = j*255/800;
+			IOWR_16DIRECT(image_base,j+i*image_height,v); /*checar depois tamanho ponteiro (e internamente castado pra alt_u8)*/
+			DEBUGF("wrote %x at %x", v, j);
+		}
+	}
 }
