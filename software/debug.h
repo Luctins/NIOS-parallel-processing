85177675ba652f03ab491c7d7679457fda682036
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-03-19 19:59:19 -0300

    add debug macros

--- software/debug.h
+++ software/debug.h
@@ -1,10 +1,31 @@
-#ifdef DEBUG_H
+#ifndef DEBUG_H
 #define DEBUG_H
 
-#define DEBUG_OUTPUT alt_printf
+/**
+ * @file debug.h 
+ * @author Lucas Martins Mendes
+ * @brief debug macros
+ */
 
-#define DEBUG(fmt,...)                             \
-  DEBUG_OUTPUT(fmt,__VA_ARGS__)
+#define DEBUG_OUTPUT_F alt_printf
+#define DEBUG_OUTPUT alt_putstr
+
+#define DEBUG(msg) 						\
+	DEBUG_OUTPUT(msg);					\
+	DEBUG_OUTPUT("\n")
+
+#define DEBUGF(msg,...)					\
+	DEBUG_OUTPUT_F(msg, __VA_ARGS__ );	\
+	DEBUG_OUTPUT("\n")
+
+#define PRINT_NUM(str,number)  		\
+	DEBUG_OUTPUT(str);  			\
+	print_num(number);             	\
+	DEBUG_OUTPUT("\n")
+
+#define VAR_DUMP(var,fmt)         	\
+	DEBUG_OUTPUT_F(#var":"fmt,var);   \
+	DEBUG_OUTPUT("\n");
 
 
 #endif
