02e811997ed1e81ef03ff2539ca7e81f77364b56
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-04-09 20:08:21 -0300

    worked on 1 proc test
    expanded the one processor test, ready for testing with a simple
    operation

---
+++ software/1proc-image-process-proc0/dir-locals.el
@@ -0,0 +1,4 @@
+((nil . (eval . (setq flycheck-clang-include-path
+                      (list (expand-file-name ../common/)
+                            (expand-file-name ../1proc-image-process-proc0_bsp/drivers/inc)
+                            (expand-file-name ../1proc-image-process-proc0_bsp/HAL/inc))))))
