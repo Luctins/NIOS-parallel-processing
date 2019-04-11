d256d356db3291ac20d2ee31282511920454016b
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-04-11 20:54:31 -0300

    update create-NIOS-project.sh
    updated project creating tool, fixed some bugs

--- software/create-NIOS-project.sh
+++ software/create-NIOS-project.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-##
+#################################################
 # @file: create-NIOS-project.sh
 # @author: Lucas Martins Mendes
 # @date: qua mar 27 11:36:14 -03 2019
 # @brief: Script for autocreating NIOS projects
-##
+##################################################
 
 DEFAULT_PATH="/home/luctins/repo/NIOS-parallel-processing/fpga/"
 DEFAULT_PROJ_DIR="/home/luctins/repo/NIOS-parallel-processing/software"
@@ -16,6 +15,7 @@ trap _exit INT
 function _exit()
 {
     echo "exiting"
+    exit
     #rm -rf $PROJ_PATH
     #later do some cleanup maybe
 }
@@ -39,7 +39,7 @@ function pick_file()
             files[i+1]="$f"
             let "i= $i + 2"
         done
-        echo "files: ${files[@]}"
+        #echo "files: ${files[@]}"
         eval `resize`
         RSL=$(whiptail --title "file selector" --menu "Please select the $FNAME" $LINES $COLUMNS $(( $LINES - 8 )) "${files[@]}" 3>&1 1>&2 2>&3)
 
@@ -51,15 +51,15 @@ function pick_file()
         let "RSL= $RSL + $RSL"
         let "RSL= $RSL + 1"
 
-        echo "RSL: $RSL"
+        #echo "RSL: $RSL"
 
         RESULT="${files[${RSL}]}"
 
-        echo "result: $RESULT"
+        #echo "result: $RESULT"
 
         if [ -f $RESULT ] && [ $RESULT != ".." ]; then
-            break
             echo "found file : ${PWD}/${RESULT}"
+            break
         else
             cd $RESULT
         fi
@@ -72,27 +72,46 @@ function pick_file()
 #set up nios terminal
 #nios_bash
 
+#pick SOPC
 FNAME="SOPC location"
 pick_file
 SOPC_LOC=$RESULT
 
+#read project name
 echo -n "please insert the project name: "
 read project_name
 
-mkdir "${DEFAULT_PROJ_DIR}/${project_name}"
-mkdir "${DEFAULT_PROJ_DIR}/${project_name}/app"
-mkdir "${DEFAULT_PROJ_DIR}/${project_name}/bsp"
-cp template/Makefile "${DEFAULT_PROJ_DIR}/${project_name}/"
+#create project folders
+mkdir "${project_name}"
+mkdir "${project_name}/app"
+mkdir "${project_name}/bsp"
 
-cd "$DEFAULT_PROJ_DIR/${project_name}"
+#copy default Makefile
+cp -v template/Makefile "${project_name}/"
+#copy main and dir-locals
+cp -v template/main.c "${project_name}/app/"
+cp -v template/dir-locals.el "${project_name}/app/.dir-locals.el"
+
+cd "${DEFAULT_PROJ_DIR}/${project_name}"
+
+#insert project name in the makefile
+echo "inserted proj name into Makefile"
 sed -i -e "s/^PROJ_NAME :=/PROJ_NAME :=${project_name}/g" Makefile
 
-#create NIOS nios Project
-nios2-swexample-create --name="$project_name" --type=blank_project  --sopc-file="$SOPC_LOC" --bsp-dir=./bsp/ --app-dir=./app/
+#create NIOS Project
+nios2-swexample-create --name="$project_name" --type=blank_project  --sopc-file="$SOPC_LOC" --bsp-dir="./bsp/" --app-dir="./app/" --cpu-name=
 
+#make BSP and app
 cd bsp
 ./create-this-bsp
 cd ../app
-echo "void main(void) { } " >> main.c
 ./create-this-app
-sed -i -e "s/^C_SRCS :=/C_SRCS :=main.c/g" Makefile
+
+#insert main as a target into the makefile
+echo "edit app/Makefile to add target"
+sed  -i -e "s/^C_SRCS :=/C_SRCS :=main.c/g" Makefile
+make -s
+#insert include folders
+echo "edit app/Makefile to add include dir"
+sed -i -e "s/^ALT_INCLUDE_DIRS :=/ALT_INCLUDE_DIRS := \/home\/luctins\/repo\/NIOS-parallel-processing\/software\/common/g " Makefile
+echo "done"
