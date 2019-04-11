b2a21de907cd08fee71287c087c571b2aee444c7
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-04-11 20:17:42 -0300

    add template main

---
+++ software/template/main.c
@@ -0,0 +1,27 @@
+/*
+  @author:
+  @date:
+  @file:main.c
+ */
+
+/*----------- Includes ----------*/
+
+#include "system.h"
+#include "io.h"
+#include "sys/alt_stdio.h"
+#include "sys/time.h"
+#include "debug.h"
+#include "common.h"
+
+/*----------- Function predeclaration ---------*/
+
+int main(void)
+{
+
+
+  while(1)
+    {
+
+    }
+  return 0;/*never return, really*/
+}
