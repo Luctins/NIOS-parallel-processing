1d2e51eeb4ec9d4729a725eb0dd3c4063b00e89f
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-04-11 20:56:00 -0300

    add template files

---
+++ software/template/dir-locals.el
@@ -0,0 +1,5 @@
+((nil . (eval . (setq flycheck-clang-include-path
+                      (list (expand-file-name "../../common/")
+                            (expand-file-name "../bsp/drivers/inc")
+                            (expand-file-name "../bsp/")
+                            (expand-file-name "../bsp/HAL/inc"))))))
