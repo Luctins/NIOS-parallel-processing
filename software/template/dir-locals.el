794fc8cfde5d0c029298f634e2c4e64970018810
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-04-15 20:17:29 -0300

    update template dir-locals.el

--- software/template/dir-locals.el
+++ software/template/dir-locals.el
@@ -1,5 +1,6 @@
-((nil . (eval . (setq flycheck-clang-include-path
-                      (list (expand-file-name "../../common/")
-                            (expand-file-name "../bsp/drivers/inc")
-                            (expand-file-name "../bsp/")
-                            (expand-file-name "../bsp/HAL/inc"))))))
+((nil . ((eval . (setq flycheck-clang-include-path
+                       (list
+                        (expand-file-name "../../common/")
+                        (expand-file-name "../bsp/drivers/inc")
+                        (expand-file-name "../bsp/")
+                        (expand-file-name "../bsp/HAL/inc")))))))
