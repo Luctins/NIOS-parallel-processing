35e0a4f265d8b7e75b321493199740685f83dd14
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-04-15 20:36:49 -0300

    fix desynched files

--- software/1proc-mat-process-proc0/app/.dir-locals.el
+++ software/1proc-mat-process-proc0/app/.dir-locals.el
@@ -1,11 +1,6 @@
- ;; ((nil . (eval . (setq flycheck-clang-include-path
- ;;                       (list (expand-file-name "../../common/")
- ;;                            (expand-file-name "../bsp/drivers/inc")
- ;;                            (expand-file-name "../bsp/")
- ;;                            (expand-file-name "../bsp/HAL/inc"))))))
-
-((c-mode . (( flycheck-clang-include-path . (eval . (list
-                                            (expand-file-name "../../common/")
-                                            (expand-file-name "../bsp/drivers/inc")
-                                            (expand-file-name "../bsp/")
-                                            (expand-file-name "../bsp/HAL/inc")))))))
+((nil . ((eval . (setq flycheck-clang-include-path
+                       (list
+                        (expand-file-name "../../common/")
+                        (expand-file-name "../bsp/drivers/inc")
+                        (expand-file-name "../bsp/")
+                        (expand-file-name "../bsp/HAL/inc")))))))
