688eb1cee7fb8a29319d97ba151880596547bcbd
Lucas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-04-25 20:28:51 -0300

    update makefile

--- software/1proc-mat-process-proc0/app/main.c
+++ software/1proc-mat-process-proc0/app/main.c
@@ -1,10 +1,17 @@
+/*--------- include --------*/
 #include "system.h"
 #include "io.h"
 #include "sys/alt_stdio.h"
-#include <stdint.h>
+#include "alt_types.h"
+//#include "sys/alt_timestamp.h"
+#include "sys/times.h"
+//#include <stdint.h>
+#include <unistd.h>
 #include "sys/time.h"
-#include "../../common/debug.h"
-#include "../../common/common.h"
+#include "debug.h"
+#include "common.h"
+#include "altera_avalon_mutex.h"
+#include "altera_avalon_mutex_regs.h"
 
 /*OBS: the image is black and white with 1 byte of color depht*/
 /*
@@ -24,42 +31,52 @@
 #define BLOCK_H (IMG_H/2)
 
 #define IMG_THRESHOLD (127)
-#define IMAGE_BASE ((uint8_t *)SDRAM_CONTROLLER_BASE)
+#define IMAGE_BASE ((alt_u8 *)SDRAM_CONTROLLER_BASE)
 
 #define ACC_MAT(mat,y_pos,x_pos,x_size) ((mat) + (x_pos) +((y_pos)*(x_size)))
 
 /*------- Function pre-declaration --------*/
 
-void fill_image(uint8_t *, uint32_t, uint32_t); //__attribute__((always_inline));
-uint8_t read_block(uint8_t*,uint8_t *,uint32_t,uint32_t);
-uint8_t write_block(uint8_t *, uint8_t *, uint32_t, uint32_t);
-void process_block_simple(uint8_t *block,uint32_t,uint32_t,uint8_t(*operation)(uint8_t value));
+void fill_image(alt_u8 *, alt_u32 , alt_u32); //__attribute__((always_inline));
+alt_u8 read_block(alt_u8*,alt_u8 *,alt_u32,alt_u32);
+alt_u8 write_block(alt_u8 *, alt_u8 *, alt_u32, alt_u32);
+void process_block_simple(alt_u8 *block,alt_u32,alt_u32,alt_u8(*operation)(alt_u8 value));
 
-uint8_t test_thresh(uint8_t val)
+/*
+  test function used as parameter for process block simple
+ */
+alt_u8 test_thresh(alt_u8 val)
 {
   return (val>IMG_THRESHOLD?255:0);
 }
+
 /*------- Global Variables --------*/
 
 /*OBS: the image is black and white with 1 byte of color[?] depht*/
 clock_t start=0,now=0;
+alt_mutex_dev * mutex = NULL;
 
-uint8_t image_block [BLOCK_H*BLOCK_W]; /*! memory block used as buffer*/
+alt_u8 image_block [BLOCK_H*BLOCK_W]; /*! memory block used as buffer*/
 
 /*--------- Main ---------*/
 int main(void)
 {
 	DEBUG("hello there");
+  mutex = alt_mutex_open("/dev/mutex");
+  ASSERT_ERROR(mutex == NULL,/*do something*/);
+
 	while(1)
 	{
-		start = clock();
+  //start = clock();
+  /*implement time measuring with alt_timestamp*/
+  /*add mutex calls to code*/
 		fill_image((alt_u8 *) IMAGE_BASE,IMG_H,IMG_W);
 		now=clock();
 		DEBUG_NUM("time to fill mat:",now-start);
 
-    for(uint32_t h=0;h<IMG_H; h+=BLOCK_H)
+    for(alt_u32 h=0;h<IMG_H; h+=BLOCK_H)
       {
-        for(uint32_t w=0;w<IMG_W; w+= BLOCK_W)
+        for(alt_u32 w=0;w<IMG_W; w+= BLOCK_W)
           {
             err_t err;
             err = read_block(IMAGE_BASE, image_block, h, w);
@@ -81,16 +98,16 @@ int main(void)
  * only useful for operations independent of adjacent pixels ("operation" must be reentrant and idempotent).
  */
 void process_block_simple(
-                          uint8_t* block, /*!pointer to memory block*/
-                          uint32_t block_h, /*!block height*/
-                          uint32_t block_w, /*!block width*/
-                          uint8_t (*operation)(uint8_t value)/*function to apply to value*/
+                          alt_u8* block, /*!pointer to memory block*/
+                          alt_u32 block_h, /*!block height*/
+                          alt_u32 block_w, /*!block width*/
+                          alt_u8 (*operation)(alt_u8 value)/*function to apply to value*/
                           )
 {
-  uint32_t v=0,r=0;
-	for(uint32_t h=0; h<block_h; ++h)
+  alt_u32 v=0,r=0;
+	for(alt_u32 h=0; h<block_h; ++h)
     {
-    for(uint32_t w=0; w<block_w; ++w)
+    for(alt_u32 w=0; w<block_w; ++w)
       {
         v=*ACC_MAT(block,h,w,block_w); /*read the value*/
         r=operation(v);/* apply operation to it*/
@@ -102,27 +119,27 @@ void process_block_simple(
 /*
  * @brief write a block to memory
  */
-uint8_t write_block(
-                    uint8_t * image_base 	/*! base address in SDRAM*/,
-                    uint8_t * image_block 	/*! pointer to local memory block*/,
-                    uint32_t block_start_h  /*! block starting position */,
-                    uint32_t block_start_w  /*! block starting position */
+alt_u8 write_block(
+                    alt_u8 * image_base 	/*! base address in SDRAM*/,
+                    alt_u8 * image_block 	/*! pointer to local memory block*/,
+                    alt_u32 block_start_h  /*! block starting position */,
+                    alt_u32 block_start_w  /*! block starting position */
                     )
 {
-	uint32_t v = 0;
-  uint32_t block_end_h = BLOCK_H + block_start_h;
-  uint32_t block_end_w = BLOCK_W + block_start_w;
+	alt_u32 v = 0;
+  alt_u32 block_end_h = BLOCK_H + block_start_h;
+  alt_u32 block_end_w = BLOCK_W + block_start_w;
 
   /*stop at image boundaries*/
   block_end_h = block_end_h > IMG_H ? IMG_H : block_end_h;
   block_end_w = block_end_w > IMG_W ? IMG_W : block_end_w;
 
-  for(uint8_t h=block_start_h; h<BLOCK_H; ++h)
+  for(alt_u8 h=block_start_h; h<BLOCK_H; ++h)
     {
-      for(uint8_t w=block_start_w; w<BLOCK_W; w+=RW_SIZE)
+      for(alt_u8 w=block_start_w; w<BLOCK_W; w+=RW_SIZE)
         {
           v = IORD_32DIRECT(image_base,(w+h*BLOCK_W)>>2);
-          for(uint8_t t=0;t<3;t++)
+          for(alt_u8 t=0;t<3;t++)
             {
               *ACC_MAT(image_block, h, w+t, BLOCK_W) = (v & 0xff);
               //*(image_block+((w+t)+h*BLOCK_W)) = (v  & 0xff); /*extract the lower byte from v*/
@@ -136,23 +153,23 @@ uint8_t write_block(
 /*
  * @brief read a block of memory from flash.
  */
-uint8_t read_block( uint8_t * image_base 	/*! base address in SDRAM*/,
-                    uint8_t * image_block 	/*! pointer to local memory block*/,
-                    uint32_t block_start_h  /*! block starting position */,
-                    uint32_t block_start_w  /*! block starting position */
+alt_u8 read_block( alt_u8 * image_base 	/*! base address in SDRAM*/,
+                    alt_u8 * image_block 	/*! pointer to local memory block*/,
+                    alt_u32 block_start_h  /*! block starting position */,
+                    alt_u32 block_start_w  /*! block starting position */
                     )
 {
 	/*remember that the image_block is a 2d array*/
-	uint32_t v = 0;
-  uint32_t block_end_h = BLOCK_H + block_start_h;  block_end_h = block_end_h > IMG_H ? IMG_H : block_end_h;
-  uint32_t block_end_w = BLOCK_W + block_start_w;  block_end_w = block_end_w > IMG_W ? IMG_W : block_end_w;
+	alt_u32 v = 0;
+  alt_u32 block_end_h = BLOCK_H + block_start_h;  block_end_h = block_end_h > IMG_H ? IMG_H : block_end_h;
+  alt_u32 block_end_w = BLOCK_W + block_start_w;  block_end_w = block_end_w > IMG_W ? IMG_W : block_end_w;
 
-  for(uint8_t h=block_start_h; h<BLOCK_H; ++h)
+  for(alt_u8 h=block_start_h; h<BLOCK_H; ++h)
 	{
-		for(uint8_t w=block_start_w; w<BLOCK_W; w+=RW_SIZE)
+		for(alt_u8 w=block_start_w; w<BLOCK_W; w+=RW_SIZE)
 		{
 			v = IORD_32DIRECT(image_base,(w+h*BLOCK_W)>>2);
-			for(uint8_t t=0;t<3;t++)
+			for(alt_u8 t=0;t<3;t++)
 			{
         *ACC_MAT(image_block, h, w+t, BLOCK_W) = (v & 0xff);
         //*(image_block+((w+t)+h*BLOCK_W)) = (v  & 0xff); /*extract the lower byte from v*/
@@ -167,10 +184,10 @@ uint8_t read_block( uint8_t * image_base 	/*! base address in SDRAM*/,
  * @brief load a test "image" onto SDRAM
  *
  */
-void fill_image(uint8_t * image_base, uint32_t image_height, uint32_t image_width)
+void fill_image(alt_u8 * image_base, alt_u32 image_height, alt_u32 image_width)
 {
 	/*fill image with white to gray gradient*/
-	uint32_t i,j;
+	alt_u32 i,j;
 
 	for(i=0;i<image_height;++i) /*access the image as a contiguous region of memory */
 	{
@@ -178,15 +195,15 @@ void fill_image(uint8_t * image_base, uint32_t image_height, uint32_t image_widt
 		{
 #if IMG_FILL_GRADIENT == 1 /*use a black to white gradient*/
 #if RW_SIZE == 1
-			uint8_t v;
+      alt_u8 v;
 		 v = ((j*255)/image_width);
 			IOWR_8DIRECT(image_base,j+i*image_width,v);
 #elif RW_SIZE == 2
-			uint16_t v;
+			alt_u16 v;
 			v =		((j*255)/image_width) |((((j+1)*255)/image_width) << 8);
 			IOWR_16DIRECT(image_base,(j+i*image_width)>>1,v);
 #elif RW_SIZE == 4
-			uint32_t v,r;
+			alt_u32 v,r;
 			v =		((j*255)/image_width) | ((((j+1)*255)/image_width) << 8) |
 					((((j+2)*255)/image_width) << 16) |  ((((j+3)*255)/image_width) << 24);
 			IOWR_32DIRECT(image_base,(j+i*image_width)>>2,v);
